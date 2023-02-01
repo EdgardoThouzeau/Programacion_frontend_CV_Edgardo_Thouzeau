bfa5dd6ed06073b2a7cb54d8577808699be10482
EdgardoThouzeau <123385279+EdgardoThouzeau@users.noreply.github.com>
2023-01-31 22:27:49 -0300

    Delete curso frontEnd directory

--- curso frontEnd/programacion_frontend/js/cv.js
+++
@@ -1,6 +0,0 @@
-
-// Tengo un flotante y espero a que ocurra el evento para enviarlo arriba de html posicion (0,0)
-const  botonSubir=document.getElementById("botonSubir")
-
-botonSubir.addEventListener('click', () => { 
-  window.scrollTo(0, 0)})
